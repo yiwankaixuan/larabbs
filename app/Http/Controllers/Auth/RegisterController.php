975c72c5c3a68968c5b109289ab02d449ebb423f
yiwankaixuan <yiwankaixuan@gmail.com>
2017-11-29 19:59:46 +0000

    Add captcha

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -45,14 +45,18 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-    }
+     protected function validator(array $data)
+     {
+         return Validator::make($data, [
+             'name' => 'required|string|max:255',
+             'email' => 'required|string|email|max:255|unique:users',
+             'password' => 'required|string|min:6|confirmed',
+             'captcha' => 'required|captcha',
+         ], [
+             'captcha.required' => '验证码不能为空',
+             'captcha.captcha' => '请输入正确的验证码',
+         ]);
+     }
 
     /**
      * Create a new user instance after a valid registration.
